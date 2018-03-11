fb34f0a2cf81ae2e250a7157a1e2709ca00b75df
ajvidal91 <angelletti21@gmail.com>
2018-03-11 12:25:56 -0400

    added css

---
+++ README.md
@@ -0,0 +1,18 @@
+![WU TANG](wu-tang-name-gen/screenshot.png)
+
+
+# Wu Tang Fortune generator
+In this app you enter survey-like information and depending on what you enter your fortune will be told thru a wu-tang quote.
+
+
+
+
+## How It's Made:
+
+**Tech used:** HTML5, CSS3, JavaScript
+
+I used an Array to store the quotes, and a conditional to determine how the quote shold appear
+
+## Optimizations
+
+It was initially a name generator, but I felt that a fortune generator would make it stick out a little more. I love Wu-Tang so I styled the page with the traditional killa bee colors, and made the positioning centered
--- main.js
+++ main.js
@@ -1,7 +1,32 @@
 // create a Wu-Tang Clan name generator. Present the user with 5 survey questions
 // and based on those answers randomly generate their name. ES6+ and bonus points
 // for it running on a node server
-const wuTang = ["rza","gza","u-god","odb","method man","raekwon","inspectah deck","masta killa","ghostface killah"];
+const wuTang = ["Been 22 hard long years and stil struggling","It's 10 O'Clock HO where the F*** yo seed at?!?!?!",
+"Oooh babyyy I like it raaaaaw","The older gods put me on, had to rock this ",
+"Wu-Tang Killerrr Bees on a swarm! Rain on ya dollar's ass, disco dorm!",
+"Cash rules everything around me, cream get the moeny. Dolla dolla bills yall.",
+"We drove expensive whips and took world-wide trips and my dick's been sucked by the finest lips.",
+"Shacklin' the masses with drastic rap tactics, graphic displays melt the steel like blacksmiths.",
+"Catch the blast of a hype verse, my glock burst, leavin a hearse I did worse.",
+"We got stick up kids, corrupt cops and crack rocks and stray shots all of the block it stays hot.",
+"I smoke on the mic like smokin joe fraiser",
+"I'll sew you're asshole closed and keep feedin you, and feedin you.",
+"Next time come strapped with a f***in pamper!",
+"First things first man you're f***in with the worst, I'll be stickin pins in your head like a f***in nurse, I'll attack any n!gga who's slack in his mack, Come fully packed with a fat rugged stack.",
+"Unbalanced like elepahnats and ants on cee-saws, I throw raps that attack like the Japs on Pearl Harbor, MC's be out like bank robbers, fleain the scene.",
+"Be doin artists in like Cain did Abel. Now they money's gettin stuck to the gum under the table. That's what ya get when ya misuse what I invent. Your empire falls and ya lose every cent.",
+"In a room full of crackers, I might cut the cheese.",
+"thoughts travel at a tremendous speed, clouds of smoke of natural blends of weed.",
+"The dumb are mostly entrigued by the drum.",
+"Fusion of the 5 elements to search for the higher intelligence, man walk around celebant, believing in relevant..the most benevolent king.",
+"Witty unpredictable shots critical to analitical anology",
+"These friendly skies aint for you, they for me and mine, this the year of the grimy nigga, Ragtime, peep these niggas on the run, peep my clan emblem, iron lung aint got to tell you where its comin from.",
+"throw some water on your wrists, and take off that fucking cologne.",
+"od bless the dead, I dont really even like talking about that because that brother is not here to really see what it is and how we could explain it.",
+"You gonna hear a lot of loving songs in there, you gonna hear betrayal songs, you gonna hear action that takes place on some of the records ... But it all revolves around that world",
+"Cash rules everything around me."];
+
+
 const buttonPress = document.getElementById("press");
 
 // let name = document.getElementById("firstName");
@@ -22,24 +47,3 @@ buttonPress.addEventListener("click", () => {
     }
   }
 });
-
-
-/****************************old code******************************************/
-//
-// buttonPress.onclick=function wu(){
-// console.log("gdjsa")
-//  if (name === "");
-//   return ("");{
-// }else if (yoHood === "");
-//  return (""){
-// }else if (faveSong === "");
-//     return (""){
-// }else if (yoAge === "");
-//  return (""){
-// }else if (howTall === "");
-//  return (""){
-// }else{
-//   return Math.round(Math.random()*wuTang.length - 1)
-//   .innerHTML = '<h1> </h1>';
-// }
-// }
